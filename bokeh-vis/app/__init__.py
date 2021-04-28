ebf409d5dc92262189624aed1b5133e960637675
kiprono <kiprono_emmanuel@yahoo.com>
2021-04-28 19:51:02 +0300

    first commit

---
+++ bokeh-vis/app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+#from bokeh.models import ColumnDataSource
+#from bokeh.plotting import figure, output_file, show
+
+#Source = ColumnDataSource()
+
+#fig = figure(plot_height=600, plot_width=720, tooltips=[("Tile", "@title"), ("Released", "@released")])
+#fig.circle(x="x", y="y", source=source, size=8, color="color", line_color=None)
+#fig.xaxis.axis_label
+
+db = SQLAlchemy()
+
+def init_app():
+    # """ constructing the core app."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import routes 
+        db.create_all() # create tables for our data models
+
+        return app
---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body': 'lorem ipsum dolor sit maet jiba hcbnw a c sr feg  vee e r r',
+            'author':'emm kip',
+            'create_date':'04-25-2017'
+            
+        },
+        {
+            'id':2,
+            'title':'Article two',
+            'body': 'lorem ipsum dolor sit maet jiba hcbnw a c sr feg  vee e r r',
+            'author':'kip',
+            'create_date':'04-25-2017'
+            
+        },
+        {
+            'id':3,
+            'title':'Article three',
+            'body': 'lorem ipsum dolor sit maet jiba hcbnw a c sr feg  vee e r r',
+            'author':'emm kip',
+            'create_date':'04-25-2017'
+            
+        }
+    ]
+    return articles
